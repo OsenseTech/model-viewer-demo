efd5762b80c14fcf98791bd2ee1d23c531ff1a0f
claireliu14 <yuly830914@gmail.com>
2020-06-22 11:25:22 +0800

    add npm start script

--- index.js
+++ index.js
@@ -2,21 +2,13 @@
 var http = require('http');
 var fs = require('fs');
 //2.
-var server = http.createServer(function (req, resp) {
-    //3.
-    fs.readFile("index.html", function (error, pgResp) {
-        if (error) {
-            resp.writeHead(404);
-            resp.write('Contents you are looking are Not Found');
-        } else {
-            resp.writeHead(200, { 'Content-Type': 'text/html' });
-            resp.write(pgResp);
-        }
-
-        resp.end();
-    });
+fs.readFile('index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
 });
-//5.
-server.listen(5050);
-
-console.log('Server Started listening on 5050');
